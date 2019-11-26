aa95163d5cab9a5d7ccbf6e54522d546982ff1ca
Jessica Wang <wangjess@umich.edu>
2019-11-26 16:08:38 -0500

    powered my Django app normally

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DB_NAME', ''),
+        'NAME': 'bigboi_db',
         'USER': os.environ.get('DB_USER', ''),
         'PASSWORD': os.environ.get('DB_PASS', ''),
         'HOST': 'localhost',
