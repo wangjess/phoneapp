28f40871a29165a95875a74fff4b96eecf2f50fa
Jessica Wang <wangjess@umich.edu>
2019-11-27 14:20:27 -0500

    created users table and django is communicating w postgresql database yay

--- myapp/users/models.py
+++ myapp/users/models.py
@@ -2,13 +2,11 @@
 from __future__ import unicode_literals
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
-class Users(models.Model):
-    codename = models.CharField(max_length=25)
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-
-    class Meta:
-        managed = False
-        db_table = 'users'
+class User(models.Model):
+    email = models.CharField(max_length=255)
+    codename = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    profilePic = models.ImageField(upload_to='images/', blank=True)
